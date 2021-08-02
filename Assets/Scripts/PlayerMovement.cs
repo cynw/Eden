718640289a96d44d620547a4a7ed057d45949057
Chayanin Wong <chayaninwong@gmail.com>
2021-08-02 09:55:27 +0900

    Walk around a map

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody myRigidbody;
+    private Vector3 change;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.z = Input.GetAxisRaw("Vertical");
+        Debug.Log(change);
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+        }
+    }
+
+    void MoveCharacter()
+    {
+        myRigidbody.MovePosition(transform.position + change * speed * Time.deltaTime);
+    }
+}
