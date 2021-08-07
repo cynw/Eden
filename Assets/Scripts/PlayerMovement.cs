5dfa81ab1d5a87ef370ebb3977d886d72db4f8a2
Chayanin Wong <chayaninwong@gmail.com>
2021-08-07 19:43:56 +0900

    Change sphere to moving character

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,32 +4,47 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    private Animator animator;
     public float speed;
     private Rigidbody myRigidbody;
-    private Vector3 change;
     public VectorValue playerPosition;
+    public float rotationSpeed;
+    private CharacterController characterController;
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody>();
+        transform.position = playerPosition.initialValue;
+        characterController = GetComponent<CharacterController>();
         transform.position = playerPosition.initialValue;
     }
 
     // Update is called once per frame
     void Update()
     {
-        change = Vector3.zero;
-        change.x = Input.GetAxisRaw("Horizontal");
-        change.z = Input.GetAxisRaw("Vertical");
-        if (change != Vector3.zero)
+        
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+
+        float magnitude = Mathf.Clamp01(movementDirection.magnitude) * speed;
+        movementDirection.Normalize();
+
+        characterController.SimpleMove(movementDirection * magnitude);
+
+
+        if (movementDirection != Vector3.zero)
         {
-            MoveCharacter();
+            animator.SetBool("isRunning", true);
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
         }
     }
 
-    void MoveCharacter()
-    {
-        myRigidbody.MovePosition(transform.position + change * speed * Time.deltaTime);
-    }
 }
